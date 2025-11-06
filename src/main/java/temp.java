77ff153e10f24ff71e8d9b36228f95dc7d71d112
Alexander <alexanderhgoodall@gmail.com>
2025-11-06 09:04:21 -0800

    stuff

--- src/main/java/temp.java
+++ src/main/java/temp.java
@@ -1,25 +1,25 @@
-import org.jline.reader.LineReader;
-import org.jline.reader.LineReaderBuilder;
-import org.jline.terminal.Terminal;
-import org.jline.terminal.TerminalBuilder;
-
-import java.io.IOException;
-
-public class temp {
-    public static void main(String[] args) throws IOException {
-//        LangFileCollection temp = new LangFileCollection();
-//        temp.populateCollection("C:/Users/N112Student/Documents/AsCS/AlexCode/langEditor/en_US/en_US");
-        //String[] temp1 = {"C:/Users/N112Student/Documents/AsCS/AlexCode/langEditor/en_US/en_US/options.lang","a","","C:/Users/N112Student/Documents/AsCS/AlexCode/langEditor/en_US/en_US/stats.lang"};
-        //temp.populateCollection(temp1);
-
-//        LangFile temp3 = temp.loadFile("options.lang");
-//        System.out.println(temp3.getSubKey(""));
-//        temp3 = temp.loadFile("stats.lang");
-//        System.out.println(temp3.getSubKey("stat"));
-
-        //System.out.println(temp.getSubKey(""));
-
-        new TerminalHandler().createOutPutWindow();
-
-    }
-}
+//import org.jline.reader.LineReader;
+//import org.jline.reader.LineReaderBuilder;
+//import org.jline.terminal.Terminal;
+//import org.jline.terminal.TerminalBuilder;
+//
+//import java.io.IOException;
+//
+//public class temp {
+//    public static void main(String[] args) throws IOException {
+////        LangFileCollection temp = new LangFileCollection();
+////        temp.populateCollection("C:/Users/N112Student/Documents/AsCS/AlexCode/langEditor/en_US/en_US");
+//        //String[] temp1 = {"C:/Users/N112Student/Documents/AsCS/AlexCode/langEditor/en_US/en_US/options.lang","a","","C:/Users/N112Student/Documents/AsCS/AlexCode/langEditor/en_US/en_US/stats.lang"};
+//        //temp.populateCollection(temp1);
+//
+////        LangFile temp3 = temp.loadFile("options.lang");
+////        System.out.println(temp3.getSubKey(""));
+////        temp3 = temp.loadFile("stats.lang");
+////        System.out.println(temp3.getSubKey("stat"));
+//
+//        //System.out.println(temp.getSubKey(""));
+//
+//        new TerminalHandler().createOutPutWindow();
+//
+//    }
+//}
