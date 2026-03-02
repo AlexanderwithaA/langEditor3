55d84da872334644f19c3e873f34cfc5ff7139c9
AlexanderwithaA <101488091+AlexanderwithaA@users.noreply.github.com>
2026-03-02 09:50:15 -0800

    going for a builder, logic, and IO separation
    I don't know where the return type is defined for dialogs, the .showAndWait() perhaps?

--- src/main/java/WindowBuilder.java
+++ src/main/java/WindowBuilder.java
@@ -6,6 +6,7 @@ import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import windowComponents.ExportDialog;
 import windowComponents.JarSelectionBuilder;
 
 // This class's purpose is to separate all the logic of this program from the UI. Mainly to clean up the main class
@@ -83,32 +84,35 @@ public class WindowBuilder {
     }
 
     public void exportWindow() {
-        Dialog<String[]> exportDialog = new Dialog<>();
-        exportDialog.setTitle("Configure Manifest");
+//        Dialog<String[]> exportDialog = new Dialog<>();
+//        exportDialog.setTitle("Configure Manifest");
+//
+//        ButtonType confirm = new ButtonType("Confirm", ButtonBar.ButtonData.OK_DONE);
+//        TextField title = new TextField();
+//        TextField identifier = new TextField();
+//        TextField regionCode = new TextField();
+//        TextField credits = new TextField();
+//
+//        regionCode.setPromptText("en_US, ru_RU, zn_CH...");
+//        identifier.setPromptText("my_language_pack");
+//        credits.setPromptText("Comma separated list");
+//        boolean confirmDisable = false;
+//
+//        GridPane dialogPane = new GridPane();
+//        dialogPane.add(new Label("Title"), 0,0);
+//        dialogPane.add(new Label("ID"), 0,1);
+//        dialogPane.add(new Label("Region"), 0,2);
+//        dialogPane.add(new Label("Credits"), 0,3);
+//        dialogPane.add(title, 1,0);
+//        dialogPane.add(identifier,1,1);
+//        dialogPane.add(regionCode,1,2);
+//        dialogPane.add(credits,1,3);
+//
+//        exportDialog.getDialogPane().getChildren().add(dialogPane);
+//        exportDialog.getDialogPane().lookupButton(confirm).setDisable(confirmDisable);
+//        exportDialog.showAndWait();
 
-        ButtonType confirm = new ButtonType("Confirm", ButtonBar.ButtonData.OK_DONE);
-        TextField title = new TextField();
-        TextField identifier = new TextField();
-        TextField regionCode = new TextField();
-        TextField credits = new TextField();
-
-        regionCode.setPromptText("en_US, ru_RU, zn_CH...");
-        identifier.setPromptText("my_language_pack");
-        credits.setPromptText("Comma separated list");
-        boolean confirmDisable = false;
-
-        GridPane dialogPane = new GridPane();
-        dialogPane.add(new Label("Title"), 0,0);
-        dialogPane.add(new Label("ID"), 0,1);
-        dialogPane.add(new Label("Region"), 0,2);
-        dialogPane.add(new Label("Credits"), 0,3);
-        dialogPane.add(title, 1,0);
-        dialogPane.add(identifier,1,1);
-        dialogPane.add(regionCode,1,2);
-        dialogPane.add(credits,1,3);
-
-        exportDialog.getDialogPane().getChildren().add(dialogPane);
-        exportDialog.getDialogPane().lookupButton(confirm).setDisable(confirmDisable);
-        exportDialog.showAndWait();
+        ExportDialog dialog = new ExportDialog();
+        dialog.showAndWait();
     }
 }
---
+++ src/main/java/windowComponents/ExportDialog.java
@@ -0,0 +1,62 @@
+package windowComponents;
+
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+
+public class ExportDialog extends Dialog<String> {
+    private final ButtonType saveButtonType = new ButtonType("Save", ButtonBar.ButtonData.OK_DONE);
+    private final ButtonType cancelButtonType = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
+
+    public ExportDialog() {
+    setTitle("Configure Manifest");
+
+    // Set the content of the dialog
+    StackPane contentPane = new StackPane();
+
+    contentPane.setPrefSize(300, 200);
+
+    TextField title = new TextField();
+    TextField identifier = new TextField();
+    TextField regionCode = new TextField();
+    TextField credits = new TextField();
+
+    regionCode.setPromptText("en_US, ru_RU, zn_CH...");
+    identifier.setPromptText("my_language_pack");
+    credits.setPromptText("Comma separated list");
+
+    GridPane gridpane = new GridPane();
+    gridpane.add(new Label("Title"), 0,0);
+    gridpane.add(new Label("ID"), 0,1);
+    gridpane.add(new Label("Region"), 0,2);
+    gridpane.add(new Label("Credits"), 0,3);
+    gridpane.add(title, 1,0);
+    gridpane.add(identifier,1,1);
+    gridpane.add(regionCode,1,2);
+    gridpane.add(credits,1,3);
+
+    contentPane.getChildren().addAll(gridpane);
+
+    getDialogPane().setContent(contentPane);
+
+    // Add buttons to the dialog
+    getDialogPane().getButtonTypes().addAll(saveButtonType, cancelButtonType);
+
+    // Set the result converter
+    setResultConverter(buttonType -> {
+
+        if (buttonType == saveButtonType) {
+
+            // Perform actions when the "Save" button is clicked
+            return new String[]{title.getCharacters().toString(),identifier.getCharacters().toString(),regionCode.getCharacters().toString(),credits.getCharacters().toString()};
+
+        } else {
+
+            // Perform actions when the "Cancel" button is clicked or the dialog is closed
+            return null;
+
+        }
+
+    });
+    }
+}
