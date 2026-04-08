6ae98fa1c9c7c22cdfdb1754b1820f6ba5b26188
alexander <alexanderhgoodall@gmail.com>
2026-04-08 09:10:21 -0700

    todo update

--- src/main/java/org/wildedit/Main.java
+++ src/main/java/org/wildedit/Main.java
@@ -26,20 +26,3 @@ public class Main {
 // hotkeys to format text or a formatting bar
 // preview window (would be neat if I could hook into the provided game jar to use the native thing for this!)
 // organize source code better? Is it sensible?
-
-// vvv remaining tasks are all for an export dialog overhaul! vvv
-
-// todo... fix locales
-// x -use intermediate file path replacing any locale wherever a pack is still in development
-// x in the fileCollection mostly?
-// make the region picker use locales.
-
-// todo... optional id in export
-// -generate from title if left blank and indicate leaving blank is an option
-
-// todo... editable list of ppl for credits
-// -change text field into expandable number of items... list ig
-// -filter out commas or see if \ acts as an escape there so that no filtering is needed.
-
-// todo... don't close on invalid manifest for export
-// -as it sounds, which is sounding less and less clear...
